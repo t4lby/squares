356a2ecf65247e1592de918d2a606c78e331cb28
t4lby <alextalbot2@hotmail.co.uk>
2018-09-15 14:27:25 +0100

    yellow bullets in build

--- Assets/Scripts/Squares/YellowSquare.cs
+++ Assets/Scripts/Squares/YellowSquare.cs
@@ -7,10 +7,13 @@ public class YellowSquare : Square
 {
     public float BulletForce { get; set; }
 
+    public float BuildBulletDiff { get; set; }
+
     /// <summary>
     /// Tracks if triggered is held.
     /// </summary>
     private bool _TriggerHeld;
+    private float _NextBuildBulletTime;
 
     protected override void SetSquareProperties()
     {
@@ -20,32 +23,44 @@ public class YellowSquare : Square
         this.MinimumTransparency = 0.25f;
         this.RegenerationSpeed = 0.2f;
         this.BulletForce = 1000f;
+        this.BuildBulletDiff = 0.5f;
     }
 
     private void Start()
     {
         _TriggerHeld = false;
+        _NextBuildBulletTime = Time.time;
     }
 
     protected override void UpdateSquare()
     {
         if (Triggered &&
-            _TriggerHeld == false &&
-            this.Player != null)
+            _TriggerHeld == false)
         {
             var angle = this.transform.rotation.eulerAngles.z * Mathf.PI / 180;
             var bulletDirection = new Vector3(-Mathf.Sin(angle),
                                              Mathf.Cos(angle),
                                               0).normalized;
-            Factory.SpawnBullet(transform.position + Game.SquareSize * bulletDirection,
+            var bullet = Factory.SpawnBullet(transform.position + Game.SquareSize * bulletDirection,
                                 bulletDirection * BulletForce, this.Color);
-            this.GetComponent<Rigidbody>().AddForce(-bulletDirection * BulletForce);
+            if (IsBuildSquare)
+            {
+                Destroy(bullet.GetComponent<Collider>());
+                _NextBuildBulletTime = Time.time + BuildBulletDiff;
+            }
+            else
+            {
+                this.GetComponent<Rigidbody>().AddForce(-bulletDirection * BulletForce);
+            }
             _TriggerHeld = true;
         }
-
-        if (Triggered == false)
+        if (IsBuildSquare &&
+                Snapped &&
+                _NextBuildBulletTime < Time.time
+            || !Triggered)
         {
             _TriggerHeld = false;
+
         }
     }
 }
